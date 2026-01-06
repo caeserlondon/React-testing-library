c0aa71c7d2bcbcf5e7b331055d5adfe5d1f8fd09
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2026-01-06 12:10:36 +0700

    Add the main test

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,10 +1,10 @@
-import { it, expect, describe } from 'vitest';
-import { faker } from '@faker-js/faker';
+import { describe, it } from 'vitest';
+// import { faker } from '@faker-js/faker';
 import { db } from './mocks/db';
 
 describe('group', () => {
-  it('should', () => {
-    const product = db.product.create({ name: 'Apple' });
-    console.log(db.product.delete({ where: { id: { equals: product.id } }}))
-  })
-})
+	it('should', () => {
+		const product = db.product.create({ name: 'Apple' });
+		console.log(db.product.delete({ where: { id: { equals: product.id } } }));
+	});
+});
