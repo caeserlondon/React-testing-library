e83677c2c25a8641f80000c7b3c089d062924680
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2026-01-06 12:10:47 +0700

    Add router test

--- tests/Router.test.tsx
+++ tests/Router.test.tsx
@@ -1,39 +1,43 @@
-import { screen } from '@testing-library/react'
-import { navigateTo } from './utils'
+import { screen } from '@testing-library/react';
 import { db } from './mocks/db';
+import { navigateTo } from './utils';
 
 describe('Router', () => {
-  it('should render the home page for /', () => {
-    navigateTo('/');
+	it('should render the home page for /', () => {
+		navigateTo('/');
 
-    expect(screen.getByRole('heading', { name: /home/i })).toBeInTheDocument();
-  })
+		expect(screen.getByRole('heading', { name: /home/i })).toBeInTheDocument();
+	});
 
-  it('should render the products page for /products', () => {
-    navigateTo('/products');
+	it('should render the products page for /products', () => {
+		navigateTo('/products');
 
-    expect(screen.getByRole('heading', { name: /products/i })).toBeInTheDocument();
-  })
+		expect(
+			screen.getByRole('heading', { name: /products/i })
+		).toBeInTheDocument();
+	});
 
-  it('should render the product details page for /products/:id', async () => {
-    const product = db.product.create();
+	it('should render the product details page for /products/:id', async () => {
+		const product = db.product.create();
 
-    navigateTo('/products/' + product.id);
+		navigateTo('/products/' + product.id);
 
-    expect(await screen.findByRole('heading', { name: product.name })).toBeInTheDocument();
+		expect(
+			await screen.findByRole('heading', { name: product.name })
+		).toBeInTheDocument();
 
-    db.product.delete({ where: { id: { equals: product.id }}});
-  });
+		db.product.delete({ where: { id: { equals: product.id } } });
+	});
 
-  it('should render the not found page for invalid routes', () => {
-    navigateTo('/invalid-route');
+	it('should render the not found page for invalid routes', () => {
+		navigateTo('/invalid-route');
 
-    expect(screen.getByText(/not found/i)).toBeInTheDocument();
-  })
+		expect(screen.getByText(/not found/i)).toBeInTheDocument();
+	});
 
-  it('should render the admin home page for /admin', () => {
-    navigateTo('/admin');
+	it('should render the admin home page for /admin', () => {
+		navigateTo('/admin');
 
-    expect(screen.getByRole('heading', { name: /admin/i })).toBeInTheDocument();
-  })
-})
+		expect(screen.getByRole('heading', { name: /admin/i })).toBeInTheDocument();
+	});
+});
