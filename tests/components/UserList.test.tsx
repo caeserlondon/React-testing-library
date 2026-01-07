141b143c8b4b6adc7e3540e3af36b2dbd044f480
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2026-01-07 09:57:39 +0700

    Add test user list

--- tests/components/UserList.test.tsx
+++ tests/components/UserList.test.tsx
@@ -3,24 +3,24 @@ import UserList from '../../src/components/UserList';
 import { User } from '../../src/entities';
 
 describe('UserList', () => {
-  it('should render no users when the users array is empty', () => {
-    render(<UserList users={[]} />);
+	it('should render no users when the users array is empty', () => {
+		render(<UserList users={[]} />);
 
-    expect(screen.getByText(/no users/i)).toBeInTheDocument();
-  });
+		expect(screen.getByText(/no users/i)).toBeInTheDocument();
+	});
 
-  it('should render a list of users', () => {
-    const users: User[] = [
-      { id: 1, name: 'Mosh' },
-      { id: 2, name: 'John' },
-    ];
+	it('should render a list of users', () => {
+		const users: User[] = [
+			{ id: 1, name: 'Mosh' },
+			{ id: 2, name: 'John' },
+		];
 
-    render(<UserList users={users} />);
+		render(<UserList users={users} />);
 
-    users.forEach(user => {
-      const link = screen.getByRole('link', { name: user.name });
-      expect(link).toBeInTheDocument();
-      expect(link).toHaveAttribute('href', `/users/${user.id}`);
-    })
-  });
-})
+		users.forEach((user) => {
+			const link = screen.getByRole('link', { name: user.name });
+			expect(link).toBeInTheDocument();
+			expect(link).toHaveAttribute('href', `/users/${user.id}`);
+		});
+	});
+});
