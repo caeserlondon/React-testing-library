5e42fa3c8aac6a4f1917a11d0f4f35e4cd1a27ad
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2026-01-08 09:27:38 +0700

    Add test tag list

--- tests/components/TagList.test.tsx
+++ tests/components/TagList.test.tsx
@@ -1,11 +1,11 @@
-import { render, screen } from "@testing-library/react";
-import TagList from "../../src/components/TagList";
+import { render, screen } from '@testing-library/react';
+import TagList from '../../src/components/TagList';
 
-describe("TagList", () => {
-  it("should render tags", async () => {
-    render(<TagList />);
+describe('TagList', () => {
+	it('should render tags', async () => {
+		render(<TagList />);
 
-    const listItems = await screen.findAllByRole("listitem");
-    expect(listItems.length).toBeGreaterThan(0);
-  });
+		const listItems = await screen.findAllByRole('listitem');
+		expect(listItems.length).toBeGreaterThan(0);
+	});
 });
